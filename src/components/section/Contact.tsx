7937f7688b6e4f16e00c502f54d36fdda7a3966e
matalbodev <mathieualbore@gmail.com>
2023-02-21 16:08:59 +0100

    Update (section) contact section add new feature

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -5,54 +5,33 @@ import Image from "next/image";
 import useEmail from "../../hooks/useEmail";
 import React, { FunctionComponent } from "react";
 const Contact: React.FC = () => {
-	const { emailDataMap } = useEmail();
-	const fields = ["surname", "name", "email"];
+	const fields = ["surname", "name", "email", "message"];
 	return (
 		<div
 			style={{
-				position: "relative",
-				paddingBottom: "322px",
 				zIndex: 1,
+				position: "relative",
 			}}
 		>
 			<Text type="h2" size="xl">
 				Contact
 			</Text>
 
-			<FormsHOC>
-				{fields.map((field, index) => {
-					const fieldData = emailDataMap.get(field);
-					const placeholder = fieldData?.placeholder;
-					const required = fieldData?.required;
-					const value = fieldData?.value as string;
-					const error = fieldData?.error as string;
-					const component = fieldData?.component as FunctionComponent<{
-						name: string;
-						placeholder?: string;
-						required?: boolean;
-						value: string;
-						error?: string;
-					}>;
-
-					return React.createElement(component, {
-						key: index,
-						name: field,
-						placeholder,
-						required,
-						value,
-						error,
-					});
-				})}
-			</FormsHOC>
-
-			<div
-				style={{
-					position: "absolute",
-					bottom: 0,
-					right: 0,
-				}}
-			>
-				<Image src="/rocket-final.png" width={141} height={322} alt="rocket" />
+			<div className="grid items-center">
+				<div className="grid-col size-2/3">
+					<FormsHOC fields={fields} />
+				</div>
+				<div className="grid-col size-1/3 text-center">
+					<Image
+						style={{
+							display: "inline-block",
+						}}
+						src="/rocket-final.png"
+						width={141}
+						height={322}
+						alt="rocket"
+					/>
+				</div>
 			</div>
 		</div>
 	);
