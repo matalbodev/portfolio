da62888412e418cf04e2d0ed23454e1e7425432d
matalbodev <mathieualbore@gmail.com>
2023-02-21 16:05:33 +0100

    Update (components) update components forms style and features

--- src/components/ui/Forms/HOC.tsx
+++ src/components/ui/Forms/HOC.tsx
@@ -1,14 +1,47 @@
-import FormsText from "./Text";
+import { FunctionComponent } from "react";
+import styles from "./forms.module.scss";
+import React from "react";
+import useEmail from "@/hooks/useEmail";
+import Button from "../Button/Button";
 
-const FormsHOC = ({ children }: { children: React.ReactNode }) => {
-	const handleSubmit = (event: any) => {
-		event.preventDefault();
-		// get form data
-		const formData = new FormData(event.target);
-		// get form data as object
-		const data = Object.fromEntries(formData.entries());
-	};
-	return <form onSubmit={handleSubmit}>{children}</form>;
+const FormsHOC = ({ fields }: { fields: string[] }) => {
+	const { emailDataMap, handleSubmit } = useEmail();
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className={styles["form-flex"]}>
+				{fields.map((field, index) => {
+					const fieldData = emailDataMap.get(field);
+					const placeholder = fieldData?.placeholder;
+					const required = fieldData?.required;
+					const value = fieldData?.value as string;
+					const error = fieldData?.error as string;
+					const size = fieldData?.size as string;
+					const component = fieldData?.component as FunctionComponent<{
+						name: string;
+						placeholder?: string;
+						required?: boolean;
+						value: string;
+						error?: string;
+					}>;
+					return (
+						<div key={index} className={`${styles["form-wrap"]} ${styles[`size-${size}`]}`}>
+							{React.createElement(component, {
+								name: field,
+								placeholder,
+								required,
+								value,
+								error,
+							})}
+						</div>
+					);
+				})}
+			</div>
+			<div className="text-center">
+				<Button type="submit">Envoyer</Button>
+			</div>
+		</form>
+	);
 };
 
 export default FormsHOC;
