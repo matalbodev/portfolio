b513cba8974315b059ec13cba0636a1cbf7e5236
matalbodev <mathieualbore@gmail.com>
2023-02-23 09:22:49 +0100

    Update (button) handle action and scroll to

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import Link from "next/link";
 import styles from "./button.module.scss";
 
 type ButtonProps = {
 	className?: string;
 	children: React.ReactNode;
 	type?: "submit" | "reset";
+	href?: string;
+	action?: () => void;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, type, className }) => {
-	return (
-		<button type={type || "button"} onClick={() => console.log("clicked")} className={`${styles.btn} ${styles["btn--primary"]}`}>
+const Button: React.FC<ButtonProps> = ({ children, type, className, href, action }) => {
+	return href ? (
+		<Link href={href} className={`${styles.btn} ${className}`}>
+			{children}
+		</Link>
+	) : (
+		<button type={type || "button"} onClick={action} className={`${styles.btn} ${styles["btn--primary"]}`}>
 			{children}
 		</button>
 	);
