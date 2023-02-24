920e7c97fb129703f36f58ae8797defd2d853bab
matalbodev <mathieualbore@gmail.com>
2023-02-24 15:11:58 +0100

    Update (config) api routes to be working !

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,11 +5,13 @@ const config = getConfig();
 export const API_HOST = config?.publicRuntimeConfig?.apiHost || "";
 export const API_TOKEN = config?.publicRuntimeConfig?.apiToken || "";
 
-export const fetcher = (endpoint: string, method: "GET" | "POST" | "PUT" | "DELETE") =>
-	fetch(`${API_HOST}/${endpoint}`, {
+export const fetcher = async (endpoint: string, method: "GET" | "POST" | "PUT" | "DELETE") => {
+	const res = await fetch(`${API_HOST}/${endpoint}`, {
 		method: method,
 		headers: {
 			"Content-Type": "application/json",
 			Authorization: `bearer ${API_TOKEN}`,
 		},
-	}).then((res) => res.json());
+	});
+	return await res.json();
+};
